f085c8d42e45e648181fac6aa731d9bd1327bc99
Romanlemeur <155751113+Romanlemeur@users.noreply.github.com>
2024-05-28 09:05:50 +0200

    Logout et supprimer le compte

---
+++ supprimer_compte.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include('config.php'); 
+
+if (isset($_SESSION['nom'])) {
+    $nomUtilisateur = $_SESSION['nom'];
+
+   
+    $sql = "DELETE FROM Collaborateur WHERE Nom = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $nomUtilisateur);
+
+    if ($stmt->execute()) {
+        
+        session_unset();
+        session_destroy();
+        header("Location: login.php"); 
+        exit();
+    } else {
+        echo "Erreur lors de la suppression du compte. Veuillez réessayer.";
+    }
+}
+?>
